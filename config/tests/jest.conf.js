ba0db30529c5101920cfe5290777376e85ba28fb
Eric McCormick <edm00se@gmail.com>
2018-10-27 23:38:22 -0500

    feat(refactor): overhauls the app to be an implementation of vue-parcel-starter
    implements parcel based app structure for simplicity and consistency with my other vue projects
    
    closes #1

---
+++ config/tests/jest.conf.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../../'),
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  setupFiles: ['<rootDir>/config/tests/setup'],
+  coverageDirectory: '<rootDir>/config/tests/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!**/node_modules/**',
+    '!src/main.js'
+  ]
+}
---
+++ src/App.spec.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './App.vue';
+import SoundList from './components/SoundList.vue';
+
+describe('App Component', () => {
+  let cmp, vm
+
+  beforeEach(() => {
+    cmp = Vue.extend(App) // Create a copy of the original component
+    vm = new cmp({
+      data: {}
+    }).$mount() // Instances and mounts the component
+  })
+
+  it('defines and adds the SoundList component', () => {
+    expect(Object.keys(App.components)).toContain('SoundList');
+  })
+});
