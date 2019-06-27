0e90589a8ddae79378f6f38eaed688bcdf6f950f
Eric McCormick <edm00se@gmail.com>
2019-06-27 15:26:23 -0500

    removes unnecessary filter
    if starting itself over, should start from beginning

--- src/components/Sound.vue
+++ src/components/Sound.vue
@@ -1,12 +1,10 @@
 <template>
-  <div
-    class="sound"
+  <div class="sound"
     v-bind:class="{ active: isActive }"
     v-bind:style="{ backgroundColor: color, color: textColor }"
     v-on:click="playAudio()"
     @mouseenter="updateHoverState(true)"
-    @mouseleave="updateHoverState(false)"
-  >
+    @mouseleave="updateHoverState(false)">
     {{ text }}
     <audio v-bind:src="soundPath"></audio>
   </div>
@@ -45,17 +43,15 @@ export default {
     },
     playAudio() {
       let sound = this.$el.querySelector('audio');
-      this.haltOtherAudio(sound);
+      this.haltOtherAudio();
       sound.currentTime = 0;
       sound.play();
     },
-    haltOtherAudio(mySound) {
-      [...document.querySelectorAll('audio')]
-        .filter(el => el !== mySound)
-        .forEach(el => {
-          el.pause();
-          el.currentTime = 0;
-        });
+    haltOtherAudio() {
+      [...document.querySelectorAll('audio')].forEach(el => {
+        el.pause();
+        el.currentTime = 0;
+      });
     }
   },
   props: ['sound']
