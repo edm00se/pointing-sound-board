5cf9599a93dc9e6b6836c67d2ee6fe03d9c1e29d
Eric McCormick <edm00se@gmail.com>
2022-02-03 12:00:29 -0600

    fix(sw): ensures it only loads in prod

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+if ('production' === process.env.NODE_ENV && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
