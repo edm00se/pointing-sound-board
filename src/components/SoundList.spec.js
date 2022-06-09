86cd6ad9ee2e6d7c0286724ae928367166311b0f
Eric McCormick <edm00se@users.noreply.github.com>
2022-06-09 21:49:05 +0000

    fixes failing soundlist spec
    just uses imported soundar's length to assert

--- src/components/SoundList.spec.js
+++ src/components/SoundList.spec.js
@@ -1,34 +1,34 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
 import SoundList from './SoundList.vue';
 import Sound from './Sound.vue';
 import SoundAr from '../assets/sounds.json';
 
-// create sample array, forcing coverage
-const soundArMock = vi.mock('../assets/sounds.json', () => [
-    {
-      text: 'Consensus',
-      path: 'consensus.m4a',
-      mime: 'audio/mp4',
-      color: null
-    },
-    {
-      text: 'Discuss',
-      path: 'talkAmongstYourselves.m4a',
-      mime: 'audio/mp4',
-      color: '#3F88C5'
-    }
-  ]
-);
+// // create sample array, forcing coverage
+// const soundArMock = vi.mock('../assets/sounds.json', () => [
+//     {
+//       text: 'Consensus',
+//       path: 'consensus.m4a',
+//       mime: 'audio/mp4',
+//       color: null
+//     },
+//     {
+//       text: 'Discuss',
+//       path: 'talkAmongstYourselves.m4a',
+//       mime: 'audio/mp4',
+//       color: '#3F88C5'
+//     }
+//   ]
+// );
 
 describe('SoundList', () => {
   const wrapper = mount(SoundList, {
-    props: {}
+    props: {},
   });
 
   it('constructs soundList accordingly', () => {
     expect(wrapper).toBeTruthy();
     const soundList = wrapper.findAllComponents(Sound);
-    expect(soundList).toHaveLength(2);
+    expect(soundList).toHaveLength(SoundAr.length);
   });
 });
