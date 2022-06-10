e5ac1c544094a9360c75ffed95cea96879c0148e
Eric McCormick <edm00se@users.noreply.github.com>
2022-06-10 01:21:47 +0000

    fixes unit tests
    ineligent, but functional

--- src/components/Sound.spec.js
+++ src/components/Sound.spec.js
@@ -7,32 +7,35 @@ describe('Sound', () => {
     text: 'Discuss',
     path: 'talkAmongstYourselves.m4a',
     mime: 'audio/mp4',
-    color: '#3F88C5'
+    color: '#3F88C5',
   };
 
   const vm = mount(Sound, {
     props: {
-      sound: sampleSound
-    }
+      sound: sampleSound,
+    },
   });
 
   it('renders props when passed', async () => {
-    expect(vm.sound.text).toBe('Discuss');
+    expect(vm.text()).toContain('Discuss');
   });
 
   it('provides sound data', async () => {
-    expect(vm.sound.path).toBe(sampleSound.path);
+    expect(vm.find('audio').html()).toContain(
+      `src="../clips/${sampleSound.path}"`
+    );
   });
 
   it('provides a different background on hover', async () => {
-    vm.updateHoverState(true);
-    expect(vm.isActive).toBe(true);
+    vm.componentVM.updateHoverState(true);
+    expect(vm.componentVM.isActive).toBe(true);
   });
 
   it('provides a playable interface', async () => {
-    // would prefer to use the Howl function with spy, but not working out currently
-    const spy = vi.spyOn(vm, 'playAudio');
-    vm.playAudio();
+    const spy = vi
+      .spyOn(vm.componentVM, 'playAudio')
+      .mockImplementation(() => {});
+    vm.componentVM.playAudio();
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledTimes(1);
   });
